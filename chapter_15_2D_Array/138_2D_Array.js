b377518226b53e323a69cfef176f9d0ff255447d
poojashree jagadeeshan <poojashreejagadeeshan@gmail.com>
2026-06-08 11:59:10 +0530

    2D Array

---
+++ chapter_15_2D_Array/138_2D_Array.js
@@ -0,0 +1,13 @@
+let a = [1, 2, 3];
+let grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+for(let i = 0; i < 3; i++){
+    for(let j = 0; j< 3; j++){
+        console.log(grid[i][j]);
+    }
+}
+
+//2D Array with simple example
